4f01e99d3d604b89daaa2b0b0a372001cf0590b6
Robert Turner <robturneris@gmail.com>
2022-09-05 16:18:51 -0400

    Test lint-staged

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,8 +75,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          Powered by <Image src="/vercel.svg" width={72} height={16} />
         </a>
       </footer>
     </div>
