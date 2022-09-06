e61135b3cc5733a26fb54ec76e059225d6ce943e
Robert Turner <robturneris@gmail.com>
2022-09-06 19:50:26 -0400

    Fixed return

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,20 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  ;<>
-    <Head>
-      <meta charSet="UTF-8" />
-      <meta
-        name="keywords"
-        content="school, Christian, preschool, Shenandoah Valley, Virginia, Waynesboro, Staunton, Fishersville"
-      />
-      <meta name="author" content="Valley Christian Academy" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
-    return <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="keywords"
+          content="school, Christian, preschool, Shenandoah Valley, Virginia, Waynesboro, Staunton, Fishersville"
+        />
+        <meta name="author" content="Valley Christian Academy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
