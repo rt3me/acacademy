f166afc02f653f8019ac70babcf71e1e9db09af0
Robert Turner <robturneris@gmail.com>
2022-09-06 19:31:14 -0400

    Added site wide meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  ;<>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta
+        name="keywords"
+        content="school, Christian, preschool, Shenandoah Valley, Virginia"
+      />
+      <meta name="author" content="Valley Christian Academy" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    return <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
