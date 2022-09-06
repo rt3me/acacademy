705db1dff79d414fa8c0a08f217d84428743b2aa
Robert Turner <robturneris@gmail.com>
2022-09-06 19:41:24 -0400

    Edited meta keywords

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <meta charSet="UTF-8" />
       <meta
         name="keywords"
-        content="school, Christian, preschool, Shenandoah Valley, Virginia"
+        content="school, Christian, preschool, Shenandoah Valley, Virginia, Waynesboro, Staunton, Fishersville"
       />
       <meta name="author" content="Valley Christian Academy" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
