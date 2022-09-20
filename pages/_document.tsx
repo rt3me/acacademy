183a4fc6fb20c6c871fd2dc822d0169704639e48
Robert Turner <robturneris@gmail.com>
2022-09-19 20:28:18 -0400

    Add Tailwind UI classes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html
+      className="h-full scroll-smooth bg-white antialiased [font-feature-settings:'ss01']"
+      lang="en"
+    >
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
           rel="stylesheet"
         />
       </Head>
-      <body>
+      <body className="flex h-full flex-col">
         <Main />
         <NextScript />
       </body>
