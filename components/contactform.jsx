c5675d14e210e8eba22d85125867d3a27952f57d
Robert Turner <robturneris@gmail.com>
2022-10-14 09:01:23 -0400

    Add form control and validation

--- components/contactform.jsx
+++ components/contactform.jsx
@@ -5,16 +5,88 @@ import { TextField, TextArea } from './fields'
 import Button from './button'
 
 export default function Form() {
+  // States for contact form fields
   const [fullname, setFullname] = useState('')
   const [email, setEmail] = useState('')
   const [subject, setSubject] = useState('')
   const [message, setMessage] = useState('')
 
+  //   Form validation state
+  const [errors, setErrors] = useState({})
+
+  //   Setting button text on form submission
+  const [buttonText, setButtonText] = useState('Send')
+
+  // Setting success or failure messages states
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+  const [showFailureMessage, setShowFailureMessage] = useState(false)
+
+  // Validation check method
+  const handleValidation = () => {
+    const tempErrors = {}
+    let isValid = true
+
+    if (fullname.length <= 0) {
+      tempErrors.fullname = true
+      isValid = false
+    }
+    if (email.length <= 0) {
+      tempErrors.email = true
+      isValid = false
+    }
+    if (subject.length <= 0) {
+      tempErrors.subject = true
+      isValid = false
+    }
+    if (message.length <= 0) {
+      tempErrors.message = true
+      isValid = false
+    }
+
+    setErrors({ ...tempErrors })
+    console.log('errors', errors)
+    return isValid
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const isValidForm = handleValidation()
+
+    if (isValidForm) {
+      setButtonText('Sending')
+      const res = await fetch('/api/sendgrid', {
+        body: JSON.stringify({
+          email,
+          fullname,
+          subject,
+          message,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      })
+
+      const { error } = await res.json()
+      if (error) {
+        console.log(error)
+        setShowSuccessMessage(false)
+        setShowFailureMessage(true)
+        setButtonText('Send')
+        return
+      }
+      setShowSuccessMessage(true)
+      setShowFailureMessage(false)
+      setButtonText('Send')
+    }
+    console.log(fullname, email, subject, message)
+  }
+
   return (
     <Container className="max-w-3xl py-24">
       <form
-        action="/api/form"
-        method="post"
+        onSubmit={handleSubmit}
         className="mt-10 grid grid-cols-1 gap-y-8 gap-x-6 sm:grid-cols-2"
       >
         <TextField
@@ -74,8 +146,21 @@ export default function Form() {
         />
 
         <Button color="blue">
-          <span>Send</span>
+          <span>{buttonText}</span>
         </Button>
+
+        <div className="col-span-full text-left">
+          {showSuccessMessage && (
+            <p className="my-2 text-sm font-semibold text-green-500">
+              Thank you! Your Message has been delivered.
+            </p>
+          )}
+          {showFailureMessage && (
+            <p className="text-red-500">
+              Oops! Something went wrong, please try again.
+            </p>
+          )}
+        </div>
       </form>
     </Container>
   )
