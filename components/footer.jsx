8cc70faef57c2b25e18747726406024ed450c0d3
Robert Turner <robturneris@gmail.com>
2022-10-15 00:39:20 -0400

    Removed logo from footer component

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
 
 import Container from './container'
-import Logo from './logo'
 import NavLink from './navlink'
 
 export default function Footer() {
@@ -9,7 +8,6 @@ export default function Footer() {
     <footer className="bg-slate-50">
       <Container>
         <div className="py-16">
-          <Logo className="mx-auto h-10 w-auto" />
           <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
               <NavLink href="#features">Features</NavLink>
