15958b2143d832a47a66d42bd15386f8b4c8c99f
Robert Turner <robturneris@gmail.com>
2022-10-15 04:05:44 -0400

    Updated menus

--- components/header.jsx
+++ components/header.jsx
@@ -77,11 +77,13 @@ function MobileNavigation() {
             as="div"
             className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
           >
-            <MobileNavLink href="#features">Features</MobileNavLink>
-            <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
-            <MobileNavLink href="#pricing">Pricing</MobileNavLink>
+            <MobileNavLink href="/">Home</MobileNavLink>
+            <MobileNavLink href="/about">About</MobileNavLink>
+            <MobileNavLink href="/admissions">Admissions</MobileNavLink>
             <hr className="m-2 border-slate-300/40" />
-            <MobileNavLink href="/login">Sign in</MobileNavLink>
+            <MobileNavLink href="https://form.jotform.com/222765635435057">
+              Interested?
+            </MobileNavLink>
           </Popover.Panel>
         </Transition.Child>
       </Transition.Root>
@@ -100,8 +102,8 @@ export default function Header() {
             </Link>
             <div className="hidden md:flex md:gap-x-6">
               <NavLink href="/">Home</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="/about">About</NavLink>
+              <NavLink href="/admissions">Admissions</NavLink>
             </div>
           </div>
           <div className="flex items-center gap-x-5 md:gap-x-8">
