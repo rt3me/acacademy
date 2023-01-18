c726de4832836587456d6f407914b479e215a4cd
Robert Turner <robturneris@gmail.com>
2023-01-17 23:05:13 -0500

    Updated hero text

--- components/hero.jsx
+++ components/hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <Container className="pb-20 text-center md:pt-10 md:pb-36 lg:pt-32">
       <Logo widthProp={400} heightProp={160} />
       <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-        New Christian school coming soon.
+        Augusta Christian Academy is enrolling now.
       </h1>
       <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-slate-700">
         Augusta Christian Academy starts Fall 2023, serving the Augusta county
